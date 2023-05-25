e4c6df5b878a7f3489a037aa860c877b0e15b89c
OverArchMC <106332621+OverArchMC@users.noreply.github.com>
2023-05-25 14:31:54 -0400

    Patient class creation

--- src/company/Patient.java
+++ src/company/Patient.java
@@ -1,4 +1,26 @@
 package company;
+import java.util.*;
+
 
 public class Patient {
+    private ArrayList<String> symptoms;
+
+    public Patient(){
+
+    }
+
+
+    public void input(String input){
+        // add a check to make sure the symptom is real
+        symptoms.add(input);
+    }
+
+    public String getQuestion(){
+
+    }
+
+    public String getDiagnosis(){
+
+    }
+
 }
