bc7487e034f9b3e77fcef049b6fc875f7de1a3e6
DJA25 <113637127+DJA25@users.noreply.github.com>
2023-05-25 10:52:22 -0400

    hola2

--- src/company/MainLayout.java
+++ src/company/MainLayout.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class MainLayout {
     JFrame mf = new JFrame();
@@ -13,10 +15,19 @@ public class MainLayout {
     JTextArea out;
     JButton submit = new JButton("Call Your AI Doctor With These Symptoms!");
     Patient p;
+    JPanel main = new JPanel(new GridLayout(3,1));
+    JPanel displaysymptoms = new JPanel(new GridLayout(2, 5));
+    TreeSet<String> symptoms = new TreeSet<>();
     public MainLayout(Patient p) {
+
+
+        JPanel split = new JPanel();
+        split.add(new JLabel(" Click to Remove: ", SwingConstants.CENTER), BorderLayout.WEST);
+        split.add(displaysymptoms, BorderLayout.CENTER);
+        main.add(split);
         this.p = p;
         mf.setLayout(new BorderLayout());
-        mf.setSize(800, 600);
+        mf.setSize(1200, 800);
         in = new JTextField();
         JPanel sfield = new JPanel(new BorderLayout());
         sfield.add(symptomlabel, BorderLayout.WEST);
@@ -28,16 +39,51 @@ public class MainLayout {
         add.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                p.addSymptom(in.getText());
+//                boolean valid = p.input(in.getText());
+                boolean valid = true;
+                if(valid) {
+                    symptoms.add(in.getText());
+                    updateSymptomLayout();
+                }
+                else {
+                    in.setText("Invalid Input");
+                }
             }
         });
         submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String[] questions = p.getQuestions();
+//                ArrayList<String> questions = p.getQuestions();
+                /*
+                add layout stuff
+                */
             }
         });
         mf.add(top, BorderLayout.NORTH);
+        mf.add(main, BorderLayout.CENTER);
         mf.setVisible(true);
     }
+
+    public void updateSymptomLayout() {
+        System.out.println(symptoms.size());
+        System.out.println("ran");
+        displaysymptoms.removeAll();
+        for(String s: symptoms) {
+            JButton button = new JButton(s);
+            button.addActionListener(new ActionListener() {
+                final String remove = s;
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    symptoms.remove(remove);
+                    updateSymptomLayout();
+                }
+            });
+            displaysymptoms.add(button);
+        }
+        mf.pack();
+        mf.setSize(1200, 800);
+        mf.setVisible(true);
+
+    }
+
 }
