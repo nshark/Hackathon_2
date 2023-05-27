3f6c9d0531d01eaff94b4466ec70ad7106d62eed
DJA25 <113637127+DJA25@users.noreply.github.com>
2023-05-27 15:30:33 -0400

    hola2

---
+++ src/company/ChatGPT.java
@@ -0,0 +1,11 @@
+package company;
+
+public class ChatGPT {
+    static String getInfo(String diagnosis) {
+        String result = "";
+        for(int i = 0; i<100; i++) {
+            result+="test\n";
+        }
+        return result;
+    }
+}
--- src/company/Main.java
+++ src/company/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Patient p = new Patient();
         p.setMale(true);
+        System.out.println(p.getMale());
         MainLayout layout = new MainLayout(p);
 
     }
