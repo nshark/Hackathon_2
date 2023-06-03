d5c00674720ab97ee7038eb8678b6b45513a37ce
nshark <noah@sitkoff.net>
2023-06-03 10:02:40 -0400

    implemented default return

--- src/company/ApiInterface.java
+++ src/company/ApiInterface.java
@@ -10,11 +10,68 @@ import java.util.HashMap;
 public class ApiInterface {
     static public JSONObject getDiagnoses(Patient p){
         // This is going to need to return a jsonarray, not a jsonobject
-        return null;
+        JSONParser parser = new JSONParser();
+        try {
+            return (JSONObject) parser.parse(" [  \n" +
+                    "       {  \n" +
+                    "          \"ID\":188,\n" +
+                    "          \"Name\":\"Abwehrspannung (Bauch)\"\n" +
+                    "       },\n" +
+                    "       {  \n" +
+                    "          \"ID\":238,\n" +
+                    "          \"Name\":\"Angst\"\n" +
+                    "       },\n" +
+                    "       {  \n" +
+                    "          \"ID\":97,\n" +
+                    "          \"Name\":\"Aphthen\"\n" +
+                    "       },\n" +
+                    "       {  \n" +
+                    "          \"ID\":54,\n" +
+                    "          \"Name\":\"Appetitminderung\"\n" +
+                    "       },\n" +
+                    "       {  \n" +
+                    "          \"ID\":131,\n" +
+                    "          \"Name\":\"Appetitzunahme\"\n" +
+                    "       },\n" +
+                    "       {  \n" +
+                    "          \"ID\":250,\n" +
+                    "          \"Name\":\"Atemabhängige Schmerzen\"\n" +
+                    "       },\n" +
+                    "        \n" +
+                    "    ]");
+        } catch (ParseException e) {
+            return null;
+        }
     }
     static public JSONObject getQuestions(Patient p){
         // Double check documentation to make sure of the return type
-        return null;
+        JSONParser parser =new JSONParser();
+        try {
+            return (JSONObject) parser.parse("    [  \n" +
+                    "   {  \n" +
+                    "      \"ID\":9,\n" +
+                    "      \"Name\":\"Kopfschmerzen\"\n" +
+                    "   },\n" +
+                    "   {  \n" +
+                    "      \"ID\":11,\n" +
+                    "      \"Name\":\"Fieber\"\n" +
+                    "   },\n" +
+                    "   {  \n" +
+                    "      \"ID\":44,\n" +
+                    "      \"Name\":\"Übelkeit\"\n" +
+                    "   },\n" +
+                    "   {  \n" +
+                    "      \"ID\":50,\n" +
+                    "      \"Name\":\"Durchfall\"\n" +
+                    "   },\n" +
+                    "   {  \n" +
+                    "      \"ID\":10,\n" +
+                    "      \"Name\":\"Bauchschmerzen\"\n" +
+                    "   }\n" +
+                    "]\n");
+        } catch (ParseException e) {
+            return null;
+        }
     }
     static public HashMap<String, Integer> getSymptoms(){
         try {
